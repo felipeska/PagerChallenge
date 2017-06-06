bc121d659418cfd650b1caa7a208a30eb124c9ec
Felipe Calderon Barragan <felipeskabarragan@gmail.com>
2017-06-06 13:09:09 -0500

    First commit

---
+++ app/src/main/java/com/pager/challenge/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pager.challenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle(R.string.main_activity_title);
+    }
+}
